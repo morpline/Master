f555729cb4425d30262ead47dfaddf2cd78407fe
morpline <118403086+morpline@users.noreply.github.com>
2023-07-11 09:41:39 -0700

    FIX?1

--- node-express-final/server.js
+++ node-express-final/server.js
@@ -3,5 +3,16 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use("/", require("./routes"));
+app.use("/app/", require("./routes"));
 app.use(express.static("./public"));
+
+const start = async() => {
+    try{
+        // await db(process.env.MONGO_URI);
+        app.listen(PORT, console.log(`port is ${PORT}..`));
+        console.log("Conn. to the d.");
+    } catch(err){
+        console.log("Fail. to the d.");
+    }
+}
+start();
