e03b0c2659b0e5752d9e450c6004074533907425
morpline <118403086+morpline@users.noreply.github.com>
2024-02-12 15:05:49 -0700

    init

--- Node_CRUD/routes/tasks.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const tasks = require("../controllers/tasks");
-
-router.route("/").get(tasks.getTasks).post(tasks.createTask);
-router.route("/:id").get(tasks.getTask).patch(tasks.updateTask).delete(tasks.deleteTask);
-
-module.exports = router;
